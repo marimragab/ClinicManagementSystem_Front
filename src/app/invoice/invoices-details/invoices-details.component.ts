b474ac3ffe156e0fe9e241f5a8bc5fc1b1a5fda3
marimragab <maryamragab@outlook.sa>
2023-03-21 10:31:48 +0200

    create invoice module and its components

---
+++ src/app/invoice/invoices-details/invoices-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoices-details',
+  templateUrl: './invoices-details.component.html',
+  styleUrls: ['./invoices-details.component.css']
+})
+export class InvoicesDetailsComponent {
+
+}
