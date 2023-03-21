88d402599a9bac9dbba439955ac32647ace11bb4
marimragab <maryamragab@outlook.sa>
2023-03-21 10:37:45 +0200

    add patient model data

--- src/app/models/patient.ts
+++ src/app/models/patient.ts
@@ -1,2 +1,20 @@
+export interface IAddress {
+  city: string;
+  street: string;
+  building: number;
+}
+
 export class Patient {
+  constructor(
+    public _id: number,
+    public SSN: number,
+    public firstName: string,
+    public lastName: string,
+    public age: number,
+    public address: IAddress,
+    public phone: string,
+    public email: string,
+    public password: string,
+    public availability?: boolean
+  ) {}
 }
