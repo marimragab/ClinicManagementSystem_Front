b9da16f679971db9121085c6f3fa8ab6be568c2e
marimragab <maryamragab@outlook.sa>
2023-03-21 10:29:29 +0200

    add appointment details component

--- src/app/appointment/appointment.module.ts
+++ src/app/appointment/appointment.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AddAppointmentComponent } from './add-appointment/add-appointment.component';
 import { UpdateAppointmentComponent } from './update-appointment/update-appointment.component';
 import { ListAppointmentsComponent } from './list-appointments/list-appointments.component';
+import { AppointmentDetailsComponent } from './appointment-details/appointment-details.component';
 
 @NgModule({
   declarations: [
     AddAppointmentComponent,
     UpdateAppointmentComponent,
     ListAppointmentsComponent,
+    AppointmentDetailsComponent,
   ],
   imports: [CommonModule],
   exports: [
     AddAppointmentComponent,
     UpdateAppointmentComponent,
     ListAppointmentsComponent,
+    AppointmentDetailsComponent,
   ],
 })
 export class AppointmentModule {}
