ddae277380029a220dd11e7c2e97f316709e81cf
marimragab <maryamragab@outlook.sa>
2023-03-30 23:57:28 +0200

    import material angular modules to use

--- src/app/invoice/invoice.module.ts
+++ src/app/invoice/invoice.module.ts
@@ -4,6 +4,12 @@ import { AddInvoiceComponent } from './add-invoice/add-invoice.component';
 import { UpdateInvoiceComponent } from './update-invoice/update-invoice.component';
 import { ListInvoicesComponent } from './list-invoices/list-invoices.component';
 import { InvoicesDetailsComponent } from './invoices-details/invoices-details.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,15 @@ import { InvoicesDetailsComponent } from './invoices-details/invoices-details.co
     ListInvoicesComponent,
     InvoicesDetailsComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatTableModule,
+    RouterModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+  ],
   exports: [
     AddInvoiceComponent,
     UpdateInvoiceComponent,
