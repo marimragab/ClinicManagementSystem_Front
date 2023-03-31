e926c88ced642b6944a951cf1c5b4378ed6b3063
marimragab <maryamragab@outlook.sa>
2023-03-31 09:44:34 +0200

    creating sidebar and navbar components for patient dashboard

--- src/app/patient-dashboard/patient-dashboard.module.ts
+++ src/app/patient-dashboard/patient-dashboard.module.ts
@@ -5,9 +5,11 @@ import { PatientDashboardRoutingModule } from './patient-dashboard-routing.modul
 import { PatientPageComponent } from './patient-page.component';
 import { SideBarComponent } from '../core/side-bar/side-bar.component';
 import { CoreModule } from '../core/core.module';
+import { PatientSidebarComponent } from './patient-sidebar/patient-sidebar.component';
+import { PatientNavbarComponent } from './patient-navbar/patient-navbar.component';
 
 @NgModule({
-  declarations: [PatientPageComponent],
+  declarations: [PatientPageComponent, PatientSidebarComponent, PatientNavbarComponent],
   imports: [CommonModule, PatientDashboardRoutingModule, CoreModule],
 })
 export class PatientDashboardModule {}
---
+++ src/app/patient-dashboard/patient-navbar/patient-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-navbar',
+  templateUrl: './patient-navbar.component.html',
+  styleUrls: ['./patient-navbar.component.css']
+})
+export class PatientNavbarComponent {
+
+}
