5d31a18baeefe7bbf1df77c23f000fa047a455fb
marimragab <maryamragab@outlook.sa>
2023-04-01 16:31:45 +0200

    co

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -2,20 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Appointment } from '../models/appointment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportService {
-    baseurl = 'http://localhost:8080/report/';
-    getAllAppointments() {
-        return this.http.get<Appointment[]>(this.baseurl);
-      }
-    
-    //   let date = new Date();
-    getAppointmentForDay(date: Date){
-       let d= String(date);
-    const url = `http://localhost:8080/report/appointment/day?date=${d}`;
-     
-        return this.http.get<Appointment[]>(url );
-      }
-  constructor(public http: HttpClient) { }
+  baseurl = 'http://localhost:8080/report/';
+  getAllAppointments() {
+    return this.http.get<Appointment[]>(this.baseurl);
+  }
+
+  //Get all appointments for entered date
+  getAppointmentForDay(date: string) {
+    const url = `http://localhost:8080/report/appointment/day?date=${date}`;
+    return this.http.get<Appointment[]>(url);
+  }
+
+  //Get all appointments for the current day
+  getDailyAppointments() {
+    return this.http.get<Appointment[]>(this.baseurl + 'appointment/daily');
+  }
+
+  //Get Appointments of specific clinic at current day
+  getClinicAppointmentsAtToday(id: number) {
+    return this.http.get<Appointment[]>(
+      this.baseurl + 'appointment/daily/clinic/' + id
+    );
+  }
+  constructor(public http: HttpClient) {}
 }
