89e405dd8b157d882b9615cd27c7fa25605f474d
marimragab <maryamragab@outlook.sa>
2023-04-01 10:28:14 +0200

    add the content of sidebar of patient page

--- src/app/patient-dashboard/patient-sidebar/patient-sidebar.component.ts
+++ src/app/patient-dashboard/patient-sidebar/patient-sidebar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-patient-sidebar',
   templateUrl: './patient-sidebar.component.html',
-  styleUrls: ['./patient-sidebar.component.css']
+  styleUrls: ['./patient-sidebar.component.css'],
 })
 export class PatientSidebarComponent {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
 
+  logout() {
+    localStorage.removeItem('current_user');
+  }
 }
