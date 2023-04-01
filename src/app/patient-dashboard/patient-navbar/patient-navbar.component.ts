75e9b9f2421d3016f47fcae02c8fe5f070f73fcc
marimragab <maryamragab@outlook.sa>
2023-04-01 10:27:22 +0200

    adding patient navbar content

--- src/app/patient-dashboard/patient-navbar/patient-navbar.component.ts
+++ src/app/patient-dashboard/patient-navbar/patient-navbar.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-patient-navbar',
   templateUrl: './patient-navbar.component.html',
-  styleUrls: ['./patient-navbar.component.css']
+  styleUrls: ['./patient-navbar.component.css'],
 })
 export class PatientNavbarComponent {
-
+  @Output() toggleSidenavbarForMe: EventEmitter<any> = new EventEmitter();
+  opensidenav() {
+    this.toggleSidenavbarForMe.emit();
+  }
 }
